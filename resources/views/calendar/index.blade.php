6abaa70b310ad6894dd5e77c7c6d5136dea0f930
yuuki <1991sy1218@gmail.com>
2020-03-02 13:04:49 +0000

    task

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.calendar')
 @section('title', 'カレンダー')
 @section('content')
+  <div class="row">
+    <div class="col-md-8 mx-auto">
     {!!$cal_tag!!}
+    </div>
+    </div>
     <a href="{{ url('/admin/calendar/record') }}">記録設定</a>
 @endsection
