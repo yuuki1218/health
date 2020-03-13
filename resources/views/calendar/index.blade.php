2b7115ea3dde56e760b6f126b736619724bee8f0
yuuki <1991sy1218@gmail.com>
2020-03-13 12:21:19 +0000

    task

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -5,6 +5,15 @@
     <div class="col-md-8 mx-auto">
     {!!$cal_tag!!}
     </div>
-    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-8 mx-auto">
     <a href="{{ url('/admin/calendar/record') }}">記録設定</a>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-8 mx-auto">
+      <a href="{{ url('/admin/profile/index') }}">プロフィール</a>
+    </div>
+  </div>  
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,28 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//   return view('welcome');
+// });
 
-Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function()
-    {
-        Route::get('profile/create' , 'Admin\ProfileController@add');
-        Route::get('profile/edit' , 'Admin\ProfileController@edit');
-        Route::post('profile/create' , 'Admin\ProfileController@create');
-        Route::post('profile/edit' , 'Admin\ProfileController@update');
-        Route::get('profile/index' , 'Admin\ProfileController@index');
-    });
-Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function()
-    {
-        Route::get('calendar/record' , 'Admin\CalendarController@record');
-        Route::post('calendar/record' , 'Admin\CalendarController@postrecord');
-        Route::get('calendar/edit/{id}' , 'Admin\CalendarController@edit');
-        Route::post('calendar/edit' , 'Admin\CalendarController@update');
-        Route::delete('calendar/record' , 'Admin\CalendarController@deleterecord');
-        Route::get('calendar/index' , 'Admin\CalendarController@index');
-        
-    });
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function () {
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
+    Route::get('profile/index', 'Admin\ProfileController@index');
+});
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function () {
+    Route::get('calendar/record', 'Admin\CalendarController@record');
+    Route::post('calendar/record', 'Admin\CalendarController@postrecord');
+    Route::get('calendar/edit/{id}', 'Admin\CalendarController@edit');
+    Route::post('calendar/edit', 'Admin\CalendarController@update');
+    Route::delete('calendar/record', 'Admin\CalendarController@deleterecord');
+    Route::get('calendar/index', 'Admin\CalendarController@index');
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -51,7 +48,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/profile', 'ProfileController@index');
 Route::get('/', 'CalendarController@index');
-
-
-
-
